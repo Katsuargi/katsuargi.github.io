b6f6eb8b957496a02718d2b841b57fba479f4e29
Lee <Katsuargi@gmail.com>
2018-04-05 21:07:34 -0400

    Rewritten crunch system functional.

--- js/StarfleetPartCrunch.js
+++ js/StarfleetPartCrunch.js
@@ -1,5 +1,3 @@
-// Variable / part value declarations. Contains pre-set values for testing purposes. (No one wants to input 50+ values for every test run.)
-
 var userInput = {
 	lowCutOff: 5,
 	highCutOff: 6,
@@ -144,34 +142,36 @@ var subFrame = {
 	srMod: 1,
 }
 
+
+
 var currentTorpedo;
 var numberOfPhasers = 0;
 var numberOfTorpedos = 0;
 
 var workingPhaser = {
-	effect: currentPhaser.effect * (1+3.5*Math.log10(0.7*numberOfPhasers+0.3)) * ((100-frame.size) / 100) * subFrame.cMulti,
-	weight: currentPhaser.weight,
-	phaserTotalWeight: currentPhaser.weight + currentPhaser.uWeight*numberOfPhasers,
-	sr: currentPhaser.sr * (currentPhaser.weight + currentPhaser.uWeight*numberOfPhasers) * (frame.srMod * subFrame.srMod),
-	power: (currentPhaser.power + (currentPhaser.sPower * frame.size) + (currentPhaser.uPower * numberOfPhasers)),
-	o: currentPhaser.o * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
-	e: currentPhaser.e * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
-	t: currentPhaser.o * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
+	effect: function() {return currentPhaser.effect * (1+3.5*Math.log10(0.7*numberOfPhasers+0.3)) * ((100-frame.size) / 100) * subFrame.cMulti},
+	weight: function() {return currentPhaser.weight},
+	phaserTotalWeight: function() {return currentPhaser.weight + currentPhaser.uWeight*numberOfPhasers},
+	sr: function() {return currentPhaser.sr * (currentPhaser.weight + currentPhaser.uWeight*numberOfPhasers) * (frame.srMod * subFrame.srMod)},
+	power: function() {return (currentPhaser.power + (currentPhaser.sPower * frame.size) + (currentPhaser.uPower * numberOfPhasers))},
+	o: function() {return currentPhaser.o * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
+	e: function() {return currentPhaser.e * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
+	t: function() {return currentPhaser.o * (1+Math.log10(numberOfPhasers)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
 	type: 1,
-	name: currentPhaser.name,
+	name: function() {return currentPhaser.name},
 }
 
 var workingTorpedo = {
-	effect: currentTorpedo.effect * (1+3.5*Math.log10(0.7*numberOfTorpedos+0.3)) * ((100-frame.size) / 100) * subFrame.cMulti,
-	weight: currentTorpedo.weight,
-	torpedoTotalWeight: currentTorpedo.weight + currentTorpedo.uWeight*numberOfTorpedos,
-	sr: currentTorpedo.sr * (currentTorpedo.weight + currentTorpedo.uWeight*numberOfTorpedos) * (frame.srMod * subFrame.srMod),
-	power: (currentTorpedo.power + (currentTorpedo.sPower * frame.size) + (currentTorpedo.uPower * numberOfTorpedos)),
-	o: currentTorpedo.o * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
-	e: currentTorpedo.e * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
-	t: currentTorpedo.o * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod),
-	type: 1,
-	name: currentTorpedo.name,
+	effect: function() {return currentTorpedo.effect * (1+3.5*Math.log10(0.7*numberOfTorpedos+0.3)) * ((100-frame.size) / 100) * subFrame.cMulti},
+	weight: function() {return currentTorpedo.weight},
+	torpedoTotalWeight: function() {return currentTorpedo.weight + currentTorpedo.uWeight*numberOfTorpedos},
+	sr: function() {return currentTorpedo.sr * (currentTorpedo.weight + currentTorpedo.uWeight*numberOfTorpedos) * (frame.srMod * subFrame.srMod)},
+	power: function() {return (currentTorpedo.power + (currentTorpedo.sPower * frame.size) + (currentTorpedo.uPower * numberOfTorpedos))},
+	o: function() {return currentTorpedo.o * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
+	e: function() {return currentTorpedo.e * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
+	t: function() {return currentTorpedo.o * (1+Math.log10(numberOfTorpedos)*2) * (Math.pow(frame.size, 0.7) / 2) * (frame.oMod * subFrame.oMod)},
+	type: function() {return currentTorpedo.type},
+	name: function() {return currentTorpedo.name},
 }
 
 var partArray = [part1, part2, part3, part4, part5, part6];
@@ -179,9 +179,34 @@ var phaserArray = [];
 var torpedoArray = [];
 var otherArray = [];
 
-var partTotalWeight = 0;
-var partTotalWeight2 = 0;
-var partTotalWeight3 = 0;
+var t = document.createElement("table");
+var tb = document.createElement("tbody");
+var tRow = document.createElement("tr");
+var tCell=[];
+var tCellText=[];
+
+for (var i = 0; i <= 11; i++) {
+		tCell[i] = document.createElement("td");
+}
+tCellText[0] = document.createTextNode("PName1");
+tCellText[1] = document.createTextNode("Parts1");
+tCellText[2] = document.createTextNode("PName2");
+tCellText[3] = document.createTextNode("Parts2");
+tCellText[4] = document.createTextNode("Effect");
+tCellText[5] = document.createTextNode("Weight");
+tCellText[6] = document.createTextNode("SR");
+tCellText[7] = document.createTextNode("BR");
+tCellText[8] = document.createTextNode("Officers");
+tCellText[9] = document.createTextNode("Enlisted");
+tCellText[10] = document.createTextNode("Technicians");
+tCellText[11] = document.createTextNode("Power Cost");
+for (var i = 0; i <= 11; i++) {
+		tCell[i].appendChild(tCellText[i]);
+		tRow.appendChild(tCell[i]);
+}
+tb.appendChild(tRow);
+t.appendChild(tb);
+document.getElementById("display-area").appendChild(t);
 
 function sort() {
 	for (i=0; i < partArray.length; i++){
@@ -200,8 +225,6 @@ function sort() {
 }
 
 function partMixer() {
-	var x = 0;
-	var y = 0;
 	var combinedEffect;
 	var combinedWeight = 0;
 	var combinedPower;
@@ -211,25 +234,59 @@ function partMixer() {
 	var combinedO;
 	var combinedE;
 	var combinedT;
-	var z;
-	while (combinedWeight < subFrame.maxWeight) {
+	var x = 0;
+	var y = 0;
+	currentPhaser = phaserArray[x];
+	currentTorpedo = torpedoArray[y];
+	numberOfPhasers = 1;
+	numberOfTorpedos = 0;
+	function table(){
+		var row = document.createElement("tr");
+		var cell = [];
+		var partValues = [];
+		for (var i = 0; i <= 11; i++) {
+			cell[i] = document.createElement("td");
+		}
+		partValues[0] = document.createTextNode(phaserArray[x].name);
+	    partValues[1] = document.createTextNode(numberOfPhasers);
+	    partValues[2] = document.createTextNode(torpedoArray[y].name);
+	    partValues[3] = document.createTextNode(numberOfTorpedos);
+	    partValues[4] = document.createTextNode(combinedEffect);
+	    partValues[5] = document.createTextNode(combinedWeight);
+	    partValues[6] = document.createTextNode(combinedSr);
+	    partValues[7] = document.createTextNode(combinedBr);
+	    partValues[8] = document.createTextNode(combinedO);
+	    partValues[9] = document.createTextNode(combinedE);
+	    partValues[10] = document.createTextNode(combinedT);
+	    partValues[11] = document.createTextNode(combinedPower);
+	    for (var i = 0; i <= 11; i++) {
+			cell[i].appendChild(partValues[i]);
+			row.appendChild(cell[i]);
+		}
+	    tb.appendChild(row);
+	    t.appendChild(tb);
+	    document.getElementById("display-area").appendChild(t);
+	}
+	while (x <= phaserArray.length) {
 		currentPhaser = phaserArray[x];
 		currentTorpedo = torpedoArray[y];
-		numberOfPhasers = 1;
-		numberOfTorpedos = 0;
-		combinedEffect = workingPhaser.effect + workingTorpedo.effect;
-		combinedWeight = workingPhaser.phaserTotalWeight + workingTorpedo.torpedoTotalWeight;
-		combinedPower = workingPhaser.power + workingTorpedo.power;
-		combinedEff = combinedEffect / combinedWeight;
-		combinedSr = workingPhaser.sr + workingTorpedo.sr;
-		combinedBr = combinedWeight / 10;
-		combinedO = workingPhaser.o + workingTorpedo.o;
-		combinedE = workingPhaser.e + workingTorpedo.e;
-		combinedT = workingPhaser.t + workingTorpedo.t;
+		combinedEffect = workingPhaser.effect() + workingTorpedo.effect();
+		if (combinedEffect <= userInput.highCutOff && combinedEffect >= userInput.lowCutOff){
+			combinedWeight = workingPhaser.phaserTotalWeight() + workingTorpedo.torpedoTotalWeight();
+			combinedPower = workingPhaser.power() + workingTorpedo.power();
+			combinedEff = combinedEffect / combinedWeight;
+			combinedSr = workingPhaser.sr() + workingTorpedo.sr();
+			combinedBr = combinedWeight / 10;
+			combinedO = workingPhaser.o() + workingTorpedo.o();
+			combinedE = workingPhaser.e() + workingTorpedo.e();
+			combinedT = workingPhaser.t() + workingTorpedo.t();
+			console.log(phaserArray[x].name);
+			console.log(torpedoArray[y].name);
+			table();
+		}
 		numberOfTorpedos = numberOfTorpedos + 1;
 		if (numberOfTorpedos == 13) {numberOfTorpedos = 0; y = y + 1;}
-		if (currentTorpedo == NaN) {numberOfTorpedos = 0; y = 0; x = x+1;}
-		if (currentPhaser == NaN) {break;}
-		console.log(combinedWeight);
+		if (y >= torpedoArray.length) {numberOfTorpedos = 0; y = 0; numberOfPhasers = numberOfPhasers + 1;}
+		if (numberOfPhasers == 13) {numberOfPhasers = 0; x = x+1;}
 	}
 }
